1816b3defa31336f48f60271587f070db0e9c208
WoahJo <ccnettacc@gmail.com>
2021-07-12 19:54:26 -0400

    Working on creating a custom grid. Working on the function connected to the button.

--- script.js
+++ script.js
@@ -11,9 +11,22 @@ function newGrid(rows, cols){
     for(i = 0; i < (rows * cols); i++){
         let newDiv = document.createElement('div');
         gridContainer.appendChild(newDiv).className = 'grid-item';
-    }
-        
-    
+    } 
 }
+
+function promptGid(){
+    let attempts;
+    for(attempts = 0; attempts < 3; attempts++){
+        gridSize = prompt("Enter dimensions: [Ex: 20 = 20 x 20] (Limit 100)");
+        if(gridSize > 100 || gridSize != Number){
+            alert("Must be a number below or equal to 100!")
+            attempts++
+        }
+        else{ break;}
+    }
+    newGrid(gridSize, gridSize);
+}
+
+const createGrid = document.querySelector('');
 
 newGrid(gridSize, gridSize);
