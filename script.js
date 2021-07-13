b4c6c6d4e06b4870634d2665dcf5272eacd9cddf
WoahJo <ccnettacc@gmail.com>
2021-07-13 16:16:37 -0400

    Fixing a problem causing old grid configs to stack. Must remove old grid first.

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
 
 const gridContainer = document.querySelector('#grid-container');
 const gridItem = document.querySelector('.grid-item');
-let gridSize = 8;
+let gridSize = 5;
+let custGrid;
 
 
-function newGrid(rows, cols){
+function newGrid(setGrid){
     let i;
-    gridContainer.style.setProperty('--grid-rows', rows);
-    gridContainer.style.setProperty('--grid-columns', cols);
-    for(i = 0; i < (rows * cols); i++){
+    gridContainer.style.setProperty('--grid-rows', setGrid);
+    gridContainer.style.setProperty('--grid-columns', setGrid);
+    for(i = 0; i < setGrid * setGrid; i++){
         let newDiv = document.createElement('div');
         gridContainer.appendChild(newDiv).className = 'grid-item';
+        
     } 
 }
 
-function promptGid(){
-    let attempts;
-    for(attempts = 0; attempts < 3; attempts++){
-        gridSize = prompt("Enter dimensions: [Ex: 20 = 20 x 20] (Limit 100)");
-        if(gridSize > 100 || gridSize != Number){
-            alert("Must be a number below or equal to 100!")
-            attempts++
-        }
-        else{ break;}
-    }
-    newGrid(gridSize, gridSize);
+function removeGrid(){
+    gridContainer.querySelectorAll('.grid-item');
+    gridContainer.forEach(g => g.remove());
 }
 
-const createGrid = document.querySelector('');
+function promptGrid(){
+    removeGrid();
+    custGrid = prompt("Enter dimensions: [Ex: 20 = 20 x 20] (Limit 100)");
+    custGrid = Number(custGrid);
+    console.log(custGrid);
+    if(custGrid > 100 || isNaN(custGrid) || custGrid < 2){
+        // custGrid = prompt("Enter dimensions: [Ex: 20 = 20 x 20] (Limit 100)");
+        alert("Must be a number between 2 and 100"); 
+    }
+    
+    else if(custGrid <= 100){
+        gridSize = custGrid;
+        newGrid(custGrid);
+    }
+}
 
-newGrid(gridSize, gridSize);
+
+const createGrid = document.querySelector('#create-grid');
+createGrid.addEventListener("click", function(e){
+    promptGrid();
+});
+
+newGrid(gridSize);
